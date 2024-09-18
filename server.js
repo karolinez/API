cf88ea0581d7508427432c82306b16a37855b938
Karoline Souza <82714439+karolinez@users.noreply.github.com>
2024-09-18 11:11:43 -0300

    Add files via upload
    Projeto API CRUD criado com o auxilio do canal Devclub para aprendermos mais sobre o Node.js.

---
+++ server.js
@@ -0,0 +1,88 @@
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const app = express()
+app.use(express.json())
+
+const users=[]
+
+app.post('/usuarios', async (req,res)=>{
+
+   try {
+    const newUser = await prisma.user.create({
+        data:{
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+
+
+    })
+    res.status(201).json(newUser)
+
+
+} catch(error){
+    console.error(error)
+    res.status(500).json({ error:"Erro ai criar usuario"})
+
+    }
+})
+
+
+app.put('/usuarios/:id', async (req,res)=>{
+
+  await prisma.user.update({
+    where:{
+        id: req.params.id
+    },
+        data:{
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+
+
+    })
+    res.status(201).json(req.body)
+
+})
+
+app.delete('/usuarios/:id', async (req,res)=>{
+    await prisma.user.delete({
+        where:{
+            id: req.params.id
+        }
+    })
+    res.status(200).json({message:"usuario deletado com sucesso!"})
+
+})
+
+
+//criar uma rota para devolver alguma coisa. Rotas comunicação entre front e back
+app.get('/usuarios', async (req, res)=>{
+  let users = []
+
+  if (req.query){
+    users = await prisma.user.findMany({
+        where: {
+            name: req.query.name
+        }
+    })
+  } else{
+    users = await prisma.user.findMany()
+  }
+
+
+})
+app.listen(3000)
+
+/* nossas rotas precisam de 
+ 1 tipo de rota
+ 2 endereço
+ */
+/* 
+objetivos: Criar um usuario, listar todos os usuarios,editar um usuario e deletar usuario
+Route params (get/put/delete) server para algo especifico
+*/
